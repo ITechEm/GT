d3de544470a054b017470e4fd67b7b6cbc5b152e
TechEm <you@example.com>
2025-01-14 13:46:02 +0100

    update

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -1,5 +1,5 @@
 'use client';
-import {useProfile} from "@/components/UseProfile";
+
 import Link from "next/link";
 import {redirect} from "next/navigation";
 import {useState} from "react";
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -1,7 +1,5 @@
 'use client';
-import Right from "@/components/icons/Right";
-import UserTabs from "@/components/layout/UserTabs";
-import {useProfile} from "@/components/UseProfile";
+
 import Image from "next/image";
 import Link from "next/link";
 import {useEffect, useState} from "react";
