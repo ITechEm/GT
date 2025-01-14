d45f47f68fa924e5af9d9f03494c93d21c739970
TechEm <you@example.com>
2025-01-14 14:35:05 +0100

    update

---
+++ src/app/categories/page.js
@@ -0,0 +1,50 @@
+'use client';
+import {useEffect, useState} from "react";
+import toast from "react-hot-toast";
+
+export default function CategoriesPage() {
+
+  const [categoryName, setCategoryName] = useState('');
+  const [categories, setCategories] = useState([]);
+  const [editedCategory] = useState(null);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  function fetchCategories() {
+    fetch('/api/categories').then(res => {
+      res.json().then(categories => {
+        setCategories(categories);
+      });
+    });
+  }
+
+  return (
+    <section className="mt-8 max-w-2xl mx-auto">
+      <UserTabs isAdmin={true} />
+      <form className="mt-8" onSubmit={handleCategorySubmit}>
+        <div className="flex gap-2 items-end">
+          
+          <div className="pb-2 flex gap-2">
+            
+           
+          </div>
+        </div>
+      </form>
+      <div>
+        <h2 className="mt-8 text-sm text-gray-500">Existing categories</h2>
+        {categories?.length > 0 && categories.map(c => (
+          <div
+            key={c._id}
+            className="bg-gray-100 rounded-xl p-2 px-4 flex gap-1 mb-1 items-center">
+            <div className="grow">
+              {c.name}
+            </div>
+            
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
