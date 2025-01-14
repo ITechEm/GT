5247442f7fe1bf38a7be0471ee0413863e6a57c0
TechEm <you@example.com>
2025-01-14 13:52:35 +0100

    update

--- src/app/menu-items/new/page.js
+++ src/app/menu-items/new/page.js
@@ -47,14 +47,14 @@ export default function NewMenuItemPage() {
 
   return (
     <section className="mt-8">
-      <UserTabs isAdmin={true} />
+     
       <div className="max-w-2xl mx-auto mt-8">
         <Link href={'/menu-items'} className="button">
-          <Left />
+          
           <span>Show all menu items</span>
         </Link>
       </div>
-      <MenuItemForm menuItem={null} onSubmit={handleFormSubmit} />
+     
     </section>
   );
 }
--- src/app/menu-items/page.js
+++ src/app/menu-items/page.js
@@ -27,13 +27,13 @@ export default function MenuItemsPage() {
 
   return (
     <section className="mt-8 max-w-2xl mx-auto">
-      <UserTabs isAdmin={true} />
+      
       <div className="mt-8">
         <Link
           className="button flex"
           href={'/menu-items/new'}>
           <span>Create new menu item</span>
-          <Right />
+          
         </Link>
       </div>
       <div>
